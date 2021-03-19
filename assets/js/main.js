e45a2c66556802b23891ada1615a4815c04c820f
Mjau83 <mamoni.hemlin@gmail.com>
2021-03-19 12:22:55 +0000

    Added flip card function in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+/* Code inspired from freeCodeCamp on YouTube */
+const cards = document.querySelectorAll('.card');
+
+function filpCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', filpCard));
