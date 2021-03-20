fdb3674889c476b6b4708368e557bd59bdfdf9cb
Mjau83 <mamoni.hemlin@gmail.com>
2021-03-20 15:37:11 +0000

    Added a matching function to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-/* Code inspired from freeCodeCamp on YouTube */
+// Code inspired from freeCodeCamp on YouTube
+// Flip card Code
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard; 
+
 function filpCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+  if(!hasFlippedCard){
+      hasFlippedCard = true;
+      firstCard = this;
+  } else {
+      hasFlippedCard = false;
+      secondCard = this;
+
+ // Match card code 
+  if(firstCard.dataset.framework === secondCard.dataset.framework) {
+      firstCard.removeEventListener('click', filpCard)
+      secondCard.removeEventListener('click', filpCard)
+    } else {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+    }
+  }  
 }
 
 cards.forEach(card => card.addEventListener('click', filpCard));
