c6405a32836004b38d06f1ff8e56d338c4b81f7d
Mjau83 <mamoni.hemlin@gmail.com>
2021-03-20 16:30:45 +0000

    Added a reset board function in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,14 @@
 const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard; 
 
 function filpCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+
   this.classList.add('flip');
   if(!hasFlippedCard){
       hasFlippedCard = true;
@@ -18,13 +23,30 @@ function filpCard() {
   if(firstCard.dataset.framework === secondCard.dataset.framework) {
       firstCard.removeEventListener('click', filpCard)
       secondCard.removeEventListener('click', filpCard)
+      resetBoard();
+
     } else {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard = false;
+        resetBoard();
     }, 1500);
     }
   }  
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
+}
+
+(function newBoard() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16)
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', filpCard));
